e2579ecd4913d6dab51e241140027bf1e1e7e955
Gabriel Medina Piñeyro <gmp1120@hotmail.com>
2026-03-24 03:08:01 -0300

    Implemento ::after y responsive
    Corrijo muchos problemas de unidades en % que mas adelante me romperian el diseño, uso unidades como rem mas amigables con responsive. Aprendo a usar ::after de CSS para hacer buenas animaciones y me familiarizo mas con scales y rotates. Arreglo los divs de seccion estadisticas con logos que tienen rotate con hover. Comienzo a pensar en responsive con media query, por lo que me planteo hacer que cuando los pixeles lleguen a cierto punto, los a del menu top desaparezcan y quede solo un icono, que luego en celular o tablet al apretarlo me lleve a los enlaces antes mencionados; eso cuando llegue a dar funcionalidad con js. Proximo commit me planteo ordenar el codigo que ya empieza a quedar largo y esta muy desorganizado en CSS. Tambien me familiaricé con clamp en responsive.

---
+++ Proyecto2.js
@@ -0,0 +1,14 @@
+
+
+
+ScrollReveal({
+    distance: '70px',
+    duration: 1200,
+    easing: 'ease-out',
+    reset: false,
+});
+
+ScrollReveal().reveal('.hero h2', { origin: 'bottom' });
+ScrollReveal().reveal('.hero p', { origin: 'bottom', delay: 200 });
+ScrollReveal().reveal('.hero .button-scroll', { origin: 'top', delay: 800 });
+ScrollReveal().reveal('.estadisticas', { origin: 'bottom', delay: 300});
